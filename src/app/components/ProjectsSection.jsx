8d3e03d691096c20714d0a6f707e39fb1e6541d6
Lameuhton <aurelianeg2000@gmail.com>
2024-07-22 13:16:19 +0200

    changed project 1 url

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,39 +5,44 @@ import { motion, useInView } from "framer-motion"
 
 
 const projectsData = [
-    {
-        id: 1,
-        title: "Craftify - site eCommerce",
-        description: "Ce projet de groupe propose un site e-commerce pour les artisans, offrant une plateforme pour exposer et vendre des produits faits main. Conçu avec React, Tailwind, Express et Node.js, le site garantit une interface intuitive et une infrastructure solide. Les fonctionnalités incluent la gestion des articles, des paniers et des profils utilisateurs.",
-        image: "/images/craftify-home.png",
-        gitUrl: "https://github.com/YasmineSan/The_Project",
-        previewUrl: "https://craftify-project.myportfolio.com/"
-    },
-    {
-        id: 2,
-        title: "Catalogue de jeux",
-        description: "Il s'agit d'un projet de groupe, c'est un site web qui regroupe tous les jeux vidéo en utilisant une API pour les données de jeu, permettant aux utilisateurs de trier les jeux par genre, plateforme, etc., ou de les rechercher via une barre de recherche. Réalisé avec SCSS, React et avec l'API de RAWG.",
-        image: "/images/projet-bestgamez.png",
-        gitUrl: "https://github.com/Junior-dev-Track/16-react-everyonegames-christophe-aureliane",
-        previewUrl: "https://bestgamez.netlify.app/"
-    },
-    {
-        id: 3,
-        title: "Application Todolist",
-        description: "J'ai réalisé une liste de tâches interactive qui affiche le nombre restant de tâches, permet la modification et la suppression de tâches, et conserve les tâches ajoutées lorsque la page est rechargée ou fermée. Tout cela réalisé avec React, en utilisant le local storage pour stocker les tâches.",
-        image: "/images/projet-todoapp.png",
-        gitUrl: "https://github.com/Junior-dev-Track/16-react-todolist-Lameuhton",
-        previewUrl: "https://aurelianegtodolist.netlify.app/"
-    },
-    {
-        id: 4,
-        title: "Page d'accueil responsive",
-        description: "Cet exercice a été réalisé de manière indépendante et visait à apprendre à créer une page entièrement responsive basée sur un modèle donné.",
-        image: "/images/projet-responsive.png",
-        gitUrl: "https://github.com/Lameuhton/responsive-exercice",
-        previewUrl: "https://lameuhton.github.io/responsive-exercice/"
-    },
-]
+  {
+    id: 1,
+    title: "Craftify - site eCommerce",
+    description:
+      "Ce projet de groupe propose un site e-commerce pour les artisans, offrant une plateforme pour exposer et vendre des produits faits main. Conçu avec React, Tailwind, Express et Node.js, le site garantit une interface intuitive et une infrastructure solide. Les fonctionnalités incluent la gestion des articles, des paniers et des profils utilisateurs.",
+    image: "/images/craftify-home.png",
+    gitUrl: "https://github.com/YasmineSan/The_Project",
+    previewUrl: "https://ecommerce-craftify.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Catalogue de jeux",
+    description:
+      "Il s'agit d'un projet de groupe, c'est un site web qui regroupe tous les jeux vidéo en utilisant une API pour les données de jeu, permettant aux utilisateurs de trier les jeux par genre, plateforme, etc., ou de les rechercher via une barre de recherche. Réalisé avec SCSS, React et avec l'API de RAWG.",
+    image: "/images/projet-bestgamez.png",
+    gitUrl:
+      "https://github.com/Junior-dev-Track/16-react-everyonegames-christophe-aureliane",
+    previewUrl: "https://bestgamez.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Application Todolist",
+    description:
+      "J'ai réalisé une liste de tâches interactive qui affiche le nombre restant de tâches, permet la modification et la suppression de tâches, et conserve les tâches ajoutées lorsque la page est rechargée ou fermée. Tout cela réalisé avec React, en utilisant le local storage pour stocker les tâches.",
+    image: "/images/projet-todoapp.png",
+    gitUrl: "https://github.com/Junior-dev-Track/16-react-todolist-Lameuhton",
+    previewUrl: "https://aurelianegtodolist.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Page d'accueil responsive",
+    description:
+      "Cet exercice a été réalisé de manière indépendante et visait à apprendre à créer une page entièrement responsive basée sur un modèle donné.",
+    image: "/images/projet-responsive.png",
+    gitUrl: "https://github.com/Lameuhton/responsive-exercice",
+    previewUrl: "https://lameuhton.github.io/responsive-exercice/",
+  },
+];
 
 export const ProjectsSection = () => {
     const ref = useRef(null);
