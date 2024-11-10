655a37c2feb82e161df166c1503cee3dd15e5a3b
Lameuhton <aurelianeg2000@gmail.com>
2024-11-10 16:26:00 +0100

    added filters

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline"
-import Link from 'next/link'
+import React from "react";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-export const ProjectCard= ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+export const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  previewUrl,
+  tags,
+}) => {
   return (
-    <div className='transition-transform transform hover:scale-105'>
-        <div className="h-72 md:h72 rounded-t-xl relative group" style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}>
-            <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
-                <Link href={gitUrl} className='h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link' target="_blank">
-                    <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white'/>
-                </Link>
-                <Link href={previewUrl} className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link' target="_blank">
-                    <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white'/>
-                </Link>
-            </div>
+    <div className="transition-transform transform hover:scale-105">
+      <div
+        className="h-72 md:h72 rounded-t-xl relative group"
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition duration-500">
+          <Link
+            href={gitUrl}
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
+          <Link
+            href={previewUrl}
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            target="_blank"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
         </div>
-        <div className='text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4 min-h-10'>
-            <h5 className='text-xl font-semibold mb-2'>{title}</h5>
-            <p className='text-[#ADB7BE]'>{description}</p>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4 min-h-10">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE] mb-3">{description}</p>
+        <div className="flex flex-wrap gap-2 mt-5">
+          {tags.slice(1).map((tag, index) => (
+            <span
+              key={index}
+              className="bg-primary-500 text-white py-1 px-2 rounded text-sm"
+            >
+              {tag}
+            </span>
+          ))}
         </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,52 +1,96 @@
 "use client";
 import React, { useState, useRef } from 'react'
 import { ProjectCard } from './ProjectCard'
+import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion"
 
 
 const projectsData = [
   {
     id: 1,
-    title: "Craftify - site eCommerce",
+    title: "Site du Dr. François Bernaerts",
     description:
-      "Ce projet de groupe propose un site e-commerce pour les artisans, offrant une plateforme pour exposer et vendre des produits faits main. Conçu avec React, Tailwind, Express et Node.js, le site garantit une interface intuitive et une infrastructure solide. Les fonctionnalités incluent la gestion des articles, des paniers et des profils utilisateurs.",
-    image: "/images/craftify-home.png",
+      "Réalisé lors de mon stage chez Webecode, ce site vitrine présente les services du Dr. François Bernaerts, médecin généraliste homéopathe. L'interface intuitive permet aux patients de consulter les informations et de prendre rendez-vous en ligne, améliorant l'accessibilité et l'expérience utilisateur.",
+    image: "/images/projet-docteur.png",
+    tag: ["All", "Wordpress", "Tailwind"],
     gitUrl: "https://github.com/YasmineSan/The_Project",
     previewUrl: "https://ecommerce-craftify.netlify.app/",
   },
   {
     id: 2,
-    title: "Catalogue de jeux",
+    title: "Portfolio de Colin G.",
     description:
-      "Il s'agit d'un projet de groupe, c'est un site web qui regroupe tous les jeux vidéo en utilisant une API pour les données de jeu, permettant aux utilisateurs de trier les jeux par genre, plateforme, etc., ou de les rechercher via une barre de recherche. Réalisé avec SCSS, React et avec l'API de RAWG.",
-    image: "/images/projet-bestgamez.png",
-    gitUrl:
-      "https://github.com/Junior-dev-Track/16-react-everyonegames-christophe-aureliane",
-    previewUrl: "https://bestgamez.netlify.app/",
+      "Ce portfolio est un site moderne et responsive conçu pour présenter des compétences professionnelles avec un design élégant et des animations subtiles. Ce projet démontre des compétences avancées en développement front-end et une attention au détail.",
+    image: "/images/projet-portfoliocolin.png",
+    tag: ["All", "Next", "Tailwind"],
+    gitUrl: "https://github.com/Lameuhton/atome_portfolio",
+    previewUrl: "https://www.gregoire.dev/",
   },
   {
     id: 3,
-    title: "Application Todolist",
+    title: "Site de la Sandwicherie Deliziosa",
     description:
-      "J'ai réalisé une liste de tâches interactive qui affiche le nombre restant de tâches, permet la modification et la suppression de tâches, et conserve les tâches ajoutées lorsque la page est rechargée ou fermée. Tout cela réalisé avec React, en utilisant le local storage pour stocker les tâches.",
+      "Application interactive de gestion de tâches permettant de visualiser, ajouter, modifier et supprimer des tâches, tout en conservant celles-ci après la fermeture de l'application. L'interface conviviale permet de suivre efficacement les tâches à accomplir, offrant une meilleure organisation et productivité.",
     image: "/images/projet-todoapp.png",
+    tag: ["All", "Next", "Tailwind"],
     gitUrl: "https://github.com/Lameuhton/todoapp",
     previewUrl: "https://aurelianegtodolist.netlify.app/",
   },
   {
     id: 4,
-    title: "Page d'accueil responsive",
+    title: "Site eCommerce",
     description:
-      "Cet exercice a été réalisé de manière indépendante et visait à apprendre à créer une page entièrement responsive basée sur un modèle donné.",
-    image: "/images/projet-responsive.png",
-    gitUrl: "https://github.com/Lameuhton/responsive-exercice",
-    previewUrl: "https://lameuhton.github.io/responsive-exercice/",
+      "Projet de groupe visant à créer une plateforme où les artisans peuvent exposer et vendre leurs créations faites main. Le site garantit une navigation fluide et propose des fonctionnalités complètes telles que la gestion des articles, des paniers et des profils utilisateurs, assurant une expérience client optimale.",
+    image: "/images/craftify-home.png",
+    tag: ["All", "React", "Tailwind", "Express"],
+    gitUrl: "https://github.com/YasmineSan/The_Project",
+    previewUrl: "https://ecommerce-craftify.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Landing page site immobilier",
+    description:
+      "Repris d'un design template, ce projet a été réalisé pour m'entraîner et affiner mes compétences. Cette landing page offre un design moderne et minimaliste, idéal pour une présentation immobilière efficace. Il met en avant les éléments clés et assure une navigation fluide.",
+    image: "/images/projet-panto.png",
+    tag: ["All", "Next", "Tailwind"],
+    gitUrl: "https://github.com/Lameuhton/PantoProject",
+    previewUrl: "https://panto-project.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Catalogue de jeux",
+    description:
+      "Site web regroupant tous les jeux vidéo populaires, permettant aux utilisateurs de parcourir, trier et rechercher des titres par genre, plateforme et autres critères via une barre de recherche. Il offre une expérience immersive et simplifiée pour explorer et découvrir de nouveaux jeux.",
+    image: "/images/projet-bestgamez.png",
+    tag: ["All", "React", "SCSS"],
+    gitUrl:
+      "https://github.com/Junior-dev-Track/16-react-everyonegames-christophe-aureliane",
+    previewUrl: "https://bestgamez.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Application Todolist",
+    description:
+      "Application interactive de gestion de tâches permettant de visualiser, ajouter, modifier et supprimer des tâches, tout en conservant celles-ci après la fermeture de l'application. L'interface conviviale permet de suivre efficacement les tâches à accomplir, offrant une meilleure organisation et productivité.",
+    image: "/images/projet-todoapp.png",
+    tag: ["All", "React"],
+    gitUrl: "https://github.com/Lameuhton/todoapp",
+    previewUrl: "https://aurelianegtodolist.netlify.app/",
   },
 ];
 
 export const ProjectsSection = () => {
+    const [tag, setTag] = useState("All");
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true});
+
+    const handleTagChange = (newTag) => {
+      setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) =>
+      project.tag.includes(tag)
+    );
 
     const cardVariants = {
         initial: {y: 50, opacity: 0},
@@ -54,22 +98,71 @@ export const ProjectsSection = () => {
     }
 
     return (
-        <section id="projects">
-            <h2 className='text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600 mt-4 mb-8 md:mb-12'>Projets</h2>
-            <ul ref={ref} className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12'>
-                {projectsData.map((project, index) => (
-                    <motion.li key={index} variants={cardVariants} inital="initial" animate={isInView ? "animate" : "initial"} transition= {{duration: 0.3, delay: index * 0.4}}>
-                        <ProjectCard
-                            key={project.id}
-                            title={project.title}
-                            description={project.description}
-                            imgUrl={project.image}
-                            gitUrl={project.gitUrl}
-                            previewUrl={project.previewUrl}
-                        />
-                    </motion.li>
-                ))}
-            </ul>
-        </section>
-    )
+      <section id="projects" className="mt-28">
+        <h2 className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600 mt-6 ">
+          Projets
+        </h2>
+        <div className="text-white flex flex-wrap justify-center items-center gap-2 py-10">
+          <ProjectTag
+            onClick={handleTagChange}
+            name="All"
+            isSelected={tag === "All"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="React"
+            isSelected={tag === "React"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="Next"
+            isSelected={tag === "Next"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="Wordpress"
+            isSelected={tag === "Wordpress"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="Tailwind"
+            isSelected={tag === "Tailwind"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="SCSS"
+            isSelected={tag === "SCSS"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="Express"
+            isSelected={tag === "Express"}
+          />
+        </div>
+        <ul
+          ref={ref}
+          className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12"
+        >
+          {filteredProjects.map((project, index) => (
+            <motion.li
+              key={index}
+              variants={cardVariants}
+              inital="initial"
+              animate={isInView ? "animate" : "initial"}
+              transition={{ duration: 0.3, delay: index * 0.4 }}
+            >
+              <ProjectCard
+                key={project.id}
+                title={project.title}
+                description={project.description}
+                imgUrl={project.image}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
+                tags={project.tag}
+              />
+            </motion.li>
+          ))}
+        </ul>
+      </section>
+    );
 }
